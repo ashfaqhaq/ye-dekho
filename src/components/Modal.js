87e2b645e51e09fc1234313ae9ba073fb9e77b8d
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-05-19 05:02:39 +0530

    Prettier

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,124 +1,141 @@
-import React, { useEffect, useState } from 'react'
-import  {db} from '../firebase/firebase'
+import React, { useState } from "react";
+import { db } from "../firebase/firebase";
 
-import { Button, Grid, Image, Icon, Modal, Card, Label,Segment, Header,Form,TextArea,Checkbox } from 'semantic-ui-react';
-import Rating from '@material-ui/lab/Rating';
-import { useAuth } from "../context/AuthContext"
+import {
+  Button,
+  Image,
+  Icon,
+  Modal,
+  Header,
+  Form,
+  Checkbox,
+} from "semantic-ui-react";
+import Rating from "@material-ui/lab/Rating";
+import { useAuth } from "../context/AuthContext";
+import { useHistory } from "react-router-dom";
 
+function ModalComponent({ data }) {
+  const history = useHistory();
+  const { currentUser } = useAuth();
+  const [open, setOpen] = useState(false);
 
-function ModalComponent({data}) {
+  const [value, setValue] = React.useState(0);
+  const [hover, setHover] = React.useState(-1);
 
-     const { currentUser } = useAuth()
-    const [open, setOpen] = useState(false)
+  const [isSpoiler, setIsSpoiler] = useState(false);
+  const [review, setReview] = React.useState("");
 
-    const [value, setValue] = React.useState(0);
-    const [hover, setHover] = React.useState(-1);
+  async function handleSubmit() {
+    const userRef = await db
+      .collection("users")
+      .doc(currentUser.displayName)
+      .collection("reviews");
 
-    const [isSpoiler,setIsSpoiler] = useState(false);
-    const [review, setReview] = React.useState('');
+    userRef.doc(data.imdbID).set({
+      Plot: data.Plot || "",
+      Title: data.Title || "",
+      Rating: data?.imdbRating || "",
+      Runtime: data?.Runtime || "",
+      Type: data?.Type || "",
+      Writer: data?.Writer || "",
+      Poster: data?.Poster || "",
+      Year: data?.Year || "",
+      imdbID: data?.imdbRating || "",
+      imdbRating: data?.imdbRating || "",
+      imdbVotes: data?.imdbVotes || "",
+      "no-value": data?.none || "",
+      genreArray: data?.Genre.split(",") || "",
+      isSpoiler: isSpoiler || false,
+      review: review || "",
+    });
+    setOpen(false);
+    history.push("/");
+  }
 
-    async function handleSubmit (){
-      const userRef = await db.collection('users').doc(currentUser.displayName).collection('reviews');
-      console.log(data.imdbID)
-      userRef.doc(data.imdbID).set({
-        
-        Plot: data.Plot|| '',
-        Title:data.Title|| '',
-        Rating:data?.imdbRating|| '',
-        Runtime: data?.Runtime || '',
-        Type: data?.Type || '',
-        Writer: data?.Writer || '',
-        Poster:data?.Poster || '',
-        Year: data?.Year || '',
-        imdbID: data?.imdbRating || '',
-        imdbRating: data?.imdbRating || '',
-        imdbVotes: data?.imdbVotes || '',
-        'no-value': data?.none || '',  
-        genreArray:data?.genreArray || '',
-        isSpoiler: isSpoiler || false, 
-        review:review || '',
+  const labels = {
+    0.5: "Useless",
+    1: "Bad",
+    1.5: "Poor",
+    2: "Below Average",
+    2.5: "Average",
+    3: "Ok",
+    3.5: "Good",
+    4: "Better",
+    4.5: "Excellent",
+    5: "Fantastic",
+  };
+  return (
+    <div>
+      <Modal
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        trigger={
+          <Button floated="right" color="green">
+            <Icon name="write square" /> Write a review
+          </Button>
+        }
+      >
+        <Modal.Header>{data?.Title}</Modal.Header>
+        <Modal.Content image>
+          <Image size="medium" src={data?.Poster} wrapped />
+          <Modal.Description>
+            <Form fluid>
+              <Header>Write a Review </Header>
 
-         
-      });
-      setOpen(false)
+              <h3> Describe your opinion of the movie and provide a rating </h3>
 
-    }
-
-    const labels = {
-      0.5: 'Useless',
-      1: 'Bad',
-      1.5: 'Poor',
-      2: 'Below Average',
-      2.5: 'Average',
-      3: 'Ok',
-      3.5: '',
-      4: 'Good+',
-      4.5: 'Excellent',
-      5: 'Fantastic',
-    };
-    return (
-        <div>
-                <Modal
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            trigger={<Button  floated="right"  color='green'>
-            <Icon name="write square"  /> Write a review
-             </Button>}
-            >
-      <Modal.Header>{data?.Title}</Modal.Header>
-      <Modal.Content image>
-        <Image size='medium' src={data?.Poster} wrapped />
-        <Modal.Description>
-        <Form fluid>
-          <Header>Write a Review </Header>
-         
-           <h3> Describe your opinion of the movie and provide a rating  </h3>
-         
-          <Form.Field   control='textarea' rows='3' 
-          value={review}
-          onChange={(event) => {
-           setReview(event.target.value);
-          
-          }}
-          label='Review'
-          placeholder='Tell us.. .'
-        />
-         <Rating
-        name="hover-feedback"
-        value={value}
-        precision={0.5}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        onChangeActive={(event, newHover) => {
-          setHover(newHover);
-        }}
-      />{value !== null && <span mb={2}>{labels[hover !== -1 ? hover : value]}</span>}
-         <Form.Field>
-      <Checkbox label='My review contains spoilers'  onChange={(event, newValue) => {
-        setIsSpoiler(newValue.checked);
-        }} />
-    </Form.Field>
-      </Form>
-        </Modal.Description>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button color='black' onClick={() => setOpen(false)}>
-          Nope
-        </Button>
-        <Button
-          content="Yep, Publish it"
-          labelPosition='right'
-          icon='checkmark'
-          disabled={!(value && review)}
-          onClick={() => handleSubmit()}
-          positive
-        />
-      </Modal.Actions>
-    </Modal>
-        </div>
-    )
+              <Form.Field
+                control="textarea"
+                rows="3"
+                value={review}
+                onChange={(event) => {
+                  setReview(event.target.value);
+                }}
+                label="Review"
+                placeholder="Tell us.. ."
+              />
+              <Rating
+                name="hover-feedback"
+                value={value}
+                precision={0.5}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+                onChangeActive={(event, newHover) => {
+                  setHover(newHover);
+                }}
+              />
+              {value !== null && (
+                <span mb={2}>{labels[hover !== -1 ? hover : value]}</span>
+              )}
+              <Form.Field>
+                <Checkbox
+                  label="My review contains spoilers"
+                  onChange={(event, newValue) => {
+                    setIsSpoiler(newValue.checked);
+                  }}
+                />
+              </Form.Field>
+            </Form>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="black" onClick={() => setOpen(false)}>
+            Nope
+          </Button>
+          <Button
+            content="Yep, Publish it"
+            labelPosition="right"
+            icon="checkmark"
+            disabled={!(value && review)}
+            onClick={() => handleSubmit()}
+            positive
+          />
+        </Modal.Actions>
+      </Modal>
+    </div>
+  );
 }
 
-export default ModalComponent
+export default ModalComponent;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,89 +1,70 @@
-import React, { useContext, useState, useEffect } from "react"
-import { useHistory } from 'react-router'
+import React, { useContext, useState, useEffect } from "react";
 
-import { auth } from "../firebase/firebase"
-import { db } from "../firebase/firebase"
+import { auth } from "../firebase/firebase";
+import { db } from "../firebase/firebase";
 
-const AuthContext = React.createContext()
+const AuthContext = React.createContext();
 
 export function useAuth() {
-    return useContext(AuthContext)
+  return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
+  function signup(email, password, displayName) {
+    return auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: displayName,
+          })
+          .then(async () => {
+            var userDocRef = db
+              .collection("users")
+              .doc(userAuth.user.displayName);
+            userDocRef.set({
+              info: {
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName,
+              },
+            });
+          });
+      });
+  }
 
-   
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+  function logout() {
+    return auth.signOut();
+  }
 
-    const [currentUser, setCurrentUser] = useState()
-    const [isLoading, setIsLoading] = useState(true)
-    const history = useHistory()
+  useEffect(() => {
+    const subscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setIsLoading(false);
+    });
+    return subscribe;
+  }, []);
 
-    function signup(email, password, displayName) {
+  const values = {
+    currentUser,
+    login,
+    logout,
+    resetPassword,
+    signup,
+  };
 
-       
-        return    auth.createUserWithEmailAndPassword(email, password).then(userAuth => {
-           
-            userAuth.user.updateProfile({
-                displayName:displayName
-
-            }).then(async () => {
-                var userDocRef =  db.collection("users").doc(userAuth.user.displayName);
-                userDocRef.set({
-                  'info': {
-                    email: userAuth.user.email,
-                    uid: userAuth.user.uid,
-                    displayName
-                  }
-                });
-              
-              })
-          
-             
-
-
-
-
-        });
-
-    }
-
-    function login(email, password) {
-        return auth.signInWithEmailAndPassword(email, password)
-    }
-    function resetPassword(email) {
-        return auth.sendPasswordResetEmail(email)
-    }
-    function logout() {
-        return auth.signOut();
-    }
-
-    useEffect(() => {
-        const subscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user);
-            setIsLoading(false);
-         
-
-        })
-        return subscribe;
-    }, [])
-
-
-    const values = {
-        currentUser,
-        login,
-        logout,
-        resetPassword,
-        signup
-    }
-
-
-
-    return (
-        <AuthContext.Provider value={values} >
-
-            {!isLoading && children}
-        </AuthContext.Provider>
-    )
-
+  return (
+    <AuthContext.Provider value={values}>
+      {!isLoading && children}
+    </AuthContext.Provider>
+  );
 }
