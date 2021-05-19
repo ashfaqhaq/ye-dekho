bc8d2133bc4753611f5b7da9bbce5289d50ebe50
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-05-19 23:17:00 +0530

    Spoiler is being seen properly

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -15,43 +15,30 @@ import {
 } from "semantic-ui-react";
 
 function ReviewCard(props) {
-
-
-    const [data,setData] = useState(props.data);
-    useEffect(() => {
-       setData(props.data)
-    }, [props])
-    const { currentUser } = useAuth();
-   
-
-
-
+  const [data, setData] = useState(props.data);
+  useEffect(() => {
+    setData(props.data);
+  }, [props]);
+  const { currentUser } = useAuth();
 
   const [isOpen, setIsOpen] = useState(false);
   const [isSpoiler, setIsSpoiler] = useState(false);
   const [ID, setID] = useState();
 
   const open = async (id) => {
-     setID(id)
-     setIsOpen(true)
-   
-
-     
-     
-};
+    setID(id);
+    setIsOpen(true);
+  };
   const close = () => setIsOpen(false);
 
-
   function showSpoiler(id) {
-     
-    //   data(data.find(item=>item.id))
-    var obj =(data.find(item=>item.id))
-    obj.isSpoiler = false;
-   
-    console.log(obj)
-    // setData((prevState)=>[...prevState,{[obj.isSpoiler]:true}])
-    setData((prevState)=>[...prevState,obj])
-  
+    let copy = [...data];
+
+    setData(
+      copy.map((c) => (c.id === id ? { ...c, isSpoiler: !c.isSpoiler } : c))
+    );
+
+    console.log(data);
   }
 
   // eslint-disable-next-line no-unused-vars
@@ -59,7 +46,6 @@ function ReviewCard(props) {
   const handleHide = () => setIsSpoiler(false);
 
   function deleteReview(id) {
-     
     const notesRef = db.collection("users").doc(currentUser.displayName);
     notesRef.collection("reviews").doc(id).delete();
     close();
@@ -68,10 +54,10 @@ function ReviewCard(props) {
     {
       menuItem: "Review",
       render: () => (
-        <Tab.Pane style={{ boxShadow: "none" }}>
+        <Tab.Pane style={{ boxShadow: "none" }} as="h3">
           {" "}
-          <Dimmer.Dimmable as="h2" blurring dimmed={isSpoiler}>
-            <Dimmer active={isSpoiler} onClickOutside={handleHide} />
+          <Dimmer.Dimmable as="h3" blurring dimmed={data.isSpoiler}>
+            <Dimmer active={data.isSpoiler} onClickOutside={handleHide} />
             <Card.Description style={{ boxShadow: "none" }}>
               {data?.review}{" "}
             </Card.Description>
@@ -82,33 +68,34 @@ function ReviewCard(props) {
     {
       menuItem: "Plot",
       render: () => (
-        <Tab.Pane style={{ boxShadow: "none" }}>{data?.Plot}</Tab.Pane>
+        <Tab.Pane style={{ boxShadow: "none" }} as="h3"> <Card.Description style={{ boxShadow: "none" }}>  {data?.Plot} </Card.Description></Tab.Pane>
       ),
     },
   ];
 
   return (
     <Grid celled>
-      {data && data.map((data) => {
-        return (
-          <Grid.Row>
-            <Grid.Column computer={3} mobile={8} size="medium">
-              <Label attached="top" centered size="big" color="white">
-                {" "}
-                {data?.Type.toUpperCase()}
-              </Label>
-              {data?.Poster === "N/A" ? (
-                <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-              ) : (
-                <Image src={data?.Poster} size="medium" centered />
-              )}
-            </Grid.Column>
-            <Grid.Column
-              computer={13}
-              mobile={16}
-              style={{ boxShadow: "none" }}
-            >
-              {/* <Segment
+      {data &&
+        data.map((data) => {
+          return (
+            <Grid.Row>
+              <Grid.Column computer={3} mobile={8} size="medium">
+                <Label attached="top" centered size="big" color="white">
+                  {" "}
+                  {data?.Type.toUpperCase()}
+                </Label>
+                {data?.Poster === "N/A" ? (
+                  <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+                ) : (
+                  <Image src={data?.Poster} size="medium" centered />
+                )}
+              </Grid.Column>
+              <Grid.Column
+                computer={13}
+                mobile={16}
+                style={{ boxShadow: "none" }}
+              >
+                {/* <Segment
                     raised
                     fluid
                     floated="right"
@@ -123,9 +110,14 @@ function ReviewCard(props) {
                       {data?.Type.toUpperCase()}
                     </Label>
                   </Segment> */}
-              <Card fluid>
-                {data?.isSpoiler && <Button onClick={()=>showSpoiler(data.id)}> show spoiler</Button>}
-                {/* <Label
+                <Card fluid>
+                  {data?.isSpoiler && (
+                    <Button onClick={() => showSpoiler(data.id)}>
+                      {" "}
+                      show spoiler
+                    </Button>
+                  )}
+                  {/* <Label
                       as="a"
                       color="blue"
                       ribbon="right"
@@ -133,85 +125,88 @@ function ReviewCard(props) {
                     >
                       {data?.Type.toUpperCase()}
                     </Label> */}
-                {isOpen && (
-                  <Confirm
-                    open={open}
-                    onCancel={close}
-                    onConfirm={() => deleteReview(ID)}
-                  />
-                )}
-                <Card.Content fluid style={{ boxShadow: "none" }}>
-                  <Card.Header>{data?.Title}</Card.Header>
-                  <Card.Meta>{data?.Year}</Card.Meta>
-                  <Label.Group tag>
-                    <Card.Meta>
-                      {" "}
-                      Genre <Icon name="tags" size="small" />{" "}
-                      {data?.genreArray?.map((item) => (
-                        <Label as="a" color="grey">
-                          {item}
-                        </Label>
-                      ))}
-                    </Card.Meta>
-                  </Label.Group>
-                  
-                  <Tab
-                    positive
-                    menu={{ secondary: true, pointing: true }}
-                    panes={panes(data)}
-                  />
-                  <Card.Description>
-                    Plot : {data?.review} <br />
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  Rating:{" "}
-                  <Rating
-                    name="half-rating-read"
-                    defaultValue={0}
-                    size="large"
-                    value={0 + data?.Rating}
-                    precision={0.5}
-                    readOnly
-                  />
-                  <br />
-                  <Header as="h2">
-                    <a href={`https://imdb.com/title/${data?.id}`}>
-                      <Icon name="imdb" color="yellow" size="big" />
-                    </a>
-                    <Header.Content>
-                      <Rating
-                        name="half-rating-read"
-                        defaultValue={1}
-                        size="large"
-                        max={1}
-                        precision={0.5}
-                        readOnly
-                      />
-                      {data?.imdbRating}
-                      <Header.Subheader>
+                  {isOpen && (
+                    <Confirm
+                      open={open}
+                      onCancel={close}
+                      onConfirm={() => deleteReview(ID)}
+                    />
+                  )}
+                  <Card.Content fluid style={{ boxShadow: "none" }}>
+                    <Card.Header>{data?.Title}</Card.Header>
+                    <Card.Meta>{data?.Year}</Card.Meta>
+                    <Label.Group tag>
+                      <Card.Meta>
                         {" "}
-                        <Icon name="users" />
-                        {data?.imdbVotes}
-                      </Header.Subheader>
-                    </Header.Content>
-                  </Header>
-               
-                  <Button floated="right" onClick={()=>open(data.id)} negative>
-                    Delete review <Icon name="trash" size="small" />
-                  </Button>
-                </Card.Content>
+                        Genre <Icon name="tags" size="small" />{" "}
+                        {data?.genreArray?.map((item) => (
+                          <Label as="a" color="grey">
+                            {item}
+                          </Label>
+                        ))}
+                      </Card.Meta>
+                    </Label.Group>
 
-                {/* <Button onClick={open} negative>
+                    <Tab
+                      positive
+                      menu={{ secondary: true, pointing: true }}
+                      panes={panes(data)}
+                    />
+                    <Card.Description>
+                     
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    Rating:{" "}
+                    <Rating
+                      name="half-rating-read"
+                      defaultValue={0}
+                      size="large"
+                      value={0 + data?.Rating}
+                      precision={0.5}
+                      readOnly
+                    />
+                    <br />
+                    <Header as="h2">
+                      <a href={`https://imdb.com/title/${data?.id}`}>
+                        <Icon name="imdb" color="yellow" size="big" />
+                      </a>
+                      <Header.Content>
+                        <Rating
+                          name="half-rating-read"
+                          defaultValue={1}
+                          size="large"
+                          max={1}
+                          precision={0.5}
+                          readOnly
+                        />
+                        {data?.imdbRating}
+                        <Header.Subheader>
+                          {" "}
+                          <Icon name="users" />
+                          {data?.imdbVotes}
+                        </Header.Subheader>
+                      </Header.Content>
+                    </Header>
+                    <Button
+                      floated="right"
+                      onClick={() => open(data.id)}
+                      negative
+                    >
+                      Delete review <Icon name="trash" size="small" />
+                    </Button>
+                  </Card.Content>
+
+                  {/* <Button onClick={open} negative>
                       <Icon name="trash" size="big" />
                     </Button> */}
-              </Card>
+                </Card>
 
-              {/* <Image src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png' /> */}
-            </Grid.Column>
-          </Grid.Row>
-        );
-      })}
+                {/* <Image src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png' /> */}
+              </Grid.Column>
+            </Grid.Row>
+          );
+        })}
     </Grid>
   );
 }
